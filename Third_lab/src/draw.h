c02fbb701ba62731b093e65e475076d06bd6e1b7
Errerp <113059470+Errerp@users.noreply.github.com>
2024-05-29 14:58:52 +0300

    Update draw.h

--- Third_lab/src/draw.h
+++ Third_lab/src/draw.h
@@ -14,4 +14,4 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 void draw_n(SDL_Surface *s, int clipping, float scale, float move_x, float move_y, int num, float R, float a, float centerx, float centery, float alpha, int flag, int **arr, float nu, int flag2, float rotationx, float rotationy, double X_MIN, double X_MAX, double Y_MIN, double Y_MAX);
-void draw(SDL_Surface *s, int clipping, float scale, float move_x, float move_y, float nu, int num, float R, float a, float centerx, float centery, float alpha, float rotationx, float rotationy);
+void draw(SDL_Surface *s, int clipping, float scale, float move_x, float move_y, int num, float R, float a, float centerx, float centery, float alpha, float rotationx, float rotationy);
